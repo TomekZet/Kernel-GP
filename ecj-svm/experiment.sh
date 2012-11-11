0a64f2acf3e6a7ff3dc3038c199ff62ccee79d03
Tomasz Ziętkiewicz <tomek.zietkiewicz@gmail.com>
2012-11-11 20:11:29 +0100

    changes in experiment.sh script

--- ecj-svm/experiment.sh
+++ ecj-svm/experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #SBATCH --time=1
 # run with sbatch -o slurm-sbatch.out -t 4:0:0 ./experiment.sh
-python experiment.py -x 4000 -d dna --popmin 1 --popmax 101 --popstep 10 --genmin 1 --genmax 5 --genstep 2
+python experiment.py -x 4000 -c 'results/result.2012-11-11 00:47:56.args'
 #python experiment.py -c "results/result.2012-11-08 22:20:46.args"
