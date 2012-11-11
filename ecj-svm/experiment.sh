2b26dacaf2983b51b48e60e7293cb8e4e0e4ece8
Tomasz Ziętkiewicz <tomek.zietkiewicz@gmail.com>
2012-11-11 19:11:44 +0100

    new results

--- ecj-svm/experiment.sh
+++ ecj-svm/experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #SBATCH --time=1
 # run with sbatch -o slurm-sbatch.out -t 4:0:0 ./experiment.sh
-python experiment.py -x 4000 -d protein
+python experiment.py -x 4000 -d protein --popmin 11 --popmax 11 --genmin 5 --genmax 5
 #python experiment.py -c "results/result.2012-11-08 22:20:46.args"
