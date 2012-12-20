e989bdb6ffac57b44e56f63a6cb7bff1e14d1f9c
Tomasz Ziętkiewicz <tomek.zietkiewicz@gmail.com>
2012-12-20 12:18:52 +0100

    Changed path to datasets in libsvm script

--- ecj-svm/lib/libsvm-3.12/java/script.sh
+++ ecj-svm/lib/libsvm-3.12/java/script.sh
@@ -11,9 +11,11 @@ do
         do
             START=$(date +%s)
             #echo "java -classpath libsvm.jar svm_train -q -s 0 -t $i ../../../data/nowe/$dataset/$dataset.seed$s.trtst $dataset.seed$s.$i.model"
-            java -classpath libsvm.jar svm_train -q -s 0 -t $i ../../../data/nowe/$dataset/$dataset.seed$s.trtst $dataset.seed$s.$i.model
+            echo "Started training"
+            java -Xmx1300M -classpath libsvm.jar svm_train -q -s 0 -t $i ../../../data/nowe/$dataset/$dataset.seed$s.trtst $dataset.seed$s.$i.model
             #echo "java -classpath libsvm.jar svm_predict ../../../data/nowe/$dataset/$dataset.seed$s.val $dataset.seed$s.$i.model $dataset.seed$s.$i.out"
-            java -classpath libsvm.jar svm_predict ../../../data/nowe/$dataset/$dataset.seed$s.val $dataset.seed$s.$i.model $dataset.seed$s.$i.out
+            echo "Training finished, started testing"
+            java -Xmx1300M -classpath libsvm.jar svm_predict ../../../data/nowe/$dataset/$dataset.seed$s.val $dataset.seed$s.$i.model $dataset.seed$s.$i.out
 
             END=$(date +%s)
             DIFF=$(( $END - $START ))
